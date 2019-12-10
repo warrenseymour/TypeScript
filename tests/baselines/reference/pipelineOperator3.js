fd66f5d18c03e5587e992a7b1a6ea53f1a85d37b
Warren Seymour <warren@fountainhead.tech>
2019-12-10 16:47:36 +0000

    pipeline - accept baselines for pipeline contextual types

---
+++ tests/baselines/reference/pipelineOperator3.js
@@ -0,0 +1,12 @@
+//// [pipelineOperator3.ts]
+declare const xs: number[];
+declare function map<T, U>(fn: (item: T) => U): (arr: T[]) => U[];
+declare function filter<T>(fn: (item: T) => boolean): (arr: T[]) => T[];
+
+const result = xs
+    |> map(x => x.toString())
+    |> filter(x => x === '1')
+
+//// [pipelineOperator3.js]
+var _a;
+const result = (_a = xs, _a = map(x => x.toString())(_a), filter(x => x === '1')(_a));
